82caba6a2edbfd7a25b0bdade0c9bb1cb6bfe55e
omarr45 <58887202+omarr45@users.noreply.github.com>
2021-08-31 20:28:33 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+
+import LeftMenu from './components/LeftMenu';
+import MainSection from './components/MainSection';
+import RightSection from './components/RightSection';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <LeftMenu />
+      <MainSection />
+      <RightSection />
     </div>
   );
 }
